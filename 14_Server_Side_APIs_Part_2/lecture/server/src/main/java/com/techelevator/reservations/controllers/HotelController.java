86854c8b24fae4e4879f5a4f01172ff525ea8cd7
ersindeger <120711316+ersindeger@users.noreply.github.com>
2022-12-16 14:14:03 -0500

    Add files via upload

---
+++ 14_Server_Side_APIs_Part_2/lecture/server/src/main/java/com/techelevator/reservations/controllers/HotelController.java
@@ -0,0 +1,151 @@
+package com.techelevator.reservations.controllers;
+
+import com.techelevator.reservations.dao.HotelDao;
+import com.techelevator.reservations.dao.ReservationDao;
+import com.techelevator.reservations.model.Hotel;
+import com.techelevator.reservations.model.Reservation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class HotelController {
+
+    @Autowired
+    private HotelDao hotelDao;
+    @Autowired
+    private ReservationDao reservationDao;
+    //you can COMMENT OUT the @Autowired annotation above, and rather use the constructor below
+    // it will be the same thing as below.
+
+//
+//    // below is no different than a regular constructor
+//    public HotelController(HotelDao hotelDao, ReservationDao reservationDao) {
+//        this.hotelDao = hotelDao;
+//        this.reservationDao = reservationDao; }
+//
+//     public HotelController (){}
+//
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/reservations/{id}", method= RequestMethod.DELETE )
+    public void delete(@PathVariable int id){
+
+    }
+
+    /**
+     * Return All Hotels
+     *
+     * @return a list of all hotels in the system
+     */
+    @RequestMapping(path = "/hotels", method = RequestMethod.GET)
+    public List<Hotel> list() {
+        return hotelDao.list();
+    }
+
+    /**
+     * Return hotel information
+     *
+     * @param id the id of the hotel
+     * @return all info for a given hotel
+     */
+    @RequestMapping(path = "/hotels/{id}", method = RequestMethod.GET)
+    public Hotel get(@PathVariable int id) {
+        Hotel hotel = hotelDao.get(id);
+        if (hotel == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Hotel not found.");
+        } else {
+            return hotel;
+        }
+    }
+
+    /**
+     * Returns all reservations in the system
+     *
+     * @return all reservations
+     */
+    @RequestMapping(path = "/reservations", method = RequestMethod.GET)
+    public List<Reservation> listReservations() {
+        return reservationDao.findAll();
+    }
+
+    /**
+     * Get a reservation by its id
+     *
+     * @param id
+     * @return a single reservation
+     */
+    @RequestMapping(path = "reservations/{id}", method = RequestMethod.GET)
+    public Reservation getReservation(@PathVariable int id) {
+        Reservation reservation = reservationDao.get(id);
+        if (reservation == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Reservation not found.");
+        } else {
+            return reservation;
+        }
+    }
+
+    /**
+     * List of reservations by hotel
+     *
+     * @param hotelID
+     * @return all reservations for a given hotel
+     */
+    @RequestMapping(path = "/hotels/{id}/reservations", method = RequestMethod.GET)
+    public List<Reservation> listReservationsByHotel(@PathVariable("id") int hotelID) {
+        List<Reservation> reservations = reservationDao.findByHotel(hotelID);
+        if (reservations == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Hotel not found.");
+        } else {
+            return reservations;
+        }
+    }
+
+    /**
+     * Create a new reservation for a given hotel
+     *
+     * @param reservation
+     */
+    @ResponseStatus(HttpStatus.I_AM_A_TEAPOT)
+    @RequestMapping(path = "/reservations", method = RequestMethod.POST)
+    public Reservation addReservation(@Valid @RequestBody Reservation reservation) {
+        return reservationDao.create(reservation, reservation.getHotelID());
+    }
+
+    /**
+     * /hotels/filter?state=oh&city=cleveland
+     *
+     * @param state the state to filter by
+     * @param city  the city to filter by
+     * @return a list of hotels that match the city & state
+     */
+    @RequestMapping(path = "/hotels/filter", method = RequestMethod.GET)
+    public List<Hotel> filterByStateAndCity(@RequestParam String state, @RequestParam(required = false) String city) {
+
+        List<Hotel> filteredHotels = new ArrayList<>();
+        List<Hotel> hotels = list();
+
+        // return hotels that match state
+        for (Hotel hotel : hotels) {
+
+            // if city was passed we don't care about the state filter
+            if (city != null) {
+                if (hotel.getAddress().getCity().toLowerCase().equals(city.toLowerCase())) {
+                    filteredHotels.add(hotel);
+                }
+            } else {
+                if (hotel.getAddress().getState().toLowerCase().equals(state.toLowerCase())) {
+                    filteredHotels.add(hotel);
+                }
+
+            }
+        }
+
+        return filteredHotels;
+    }
+
+}
